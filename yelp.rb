12446b455f000c39fc23e3eadb62d892c7a7f2ac
aswathyd <aswathyd@gmail.com>
2015-11-19 11:10:02 -0500

    format csv file

--- yelp.rb
+++ yelp.rb
@@ -11,12 +11,12 @@ Yelp.client.configure do |config|
  end
 
 #open csv file to save search result
- CSV.open("Somerville.csv", "wb") do  |csv|
- csv <<["Name", "Brand*", "Lat*", "Lng*", "Address1", "Address2", "City", "State", "Zip code", "Country", "Phone", "Store hours", "Time zone", "Dma Code", "Status*"]
+ CSV.open("Medford.csv", "wb") do  |csv|
+ csv <<["Brand*", "Store id", "Name", "Lat*", "Lng*", "Address1", "Address2", "City", "State", "Zip code", "Country", "Phone number", "Fax number", "Store hours", "Time zone", "Dma Code", "Status*"]
 
 x = 20
 
-while x < 1001 do
+while x < 2000 do
  params = { 
  			limit: 20,
  			offset: x,
@@ -24,26 +24,36 @@ while x < 1001 do
         }
         
 #make a request to the search API
-response = Yelp.client.search('Somerville, MA', params)
+response = Yelp.client.search('Medford, MA', params)
 
 #looping through each business to get all related fields (Ex: Lat/Long, Categories etc) and saving in csv
 (0..19).each do |i|
-Name = response.businesses[i].name
-	Lat = response.businesses[i].location.coordinate.latitude
-	Long = response.businesses[i].location.coordinate.longitude
-	Brand = response.businesses[i].categories[0][0]
-	Address1 = response.businesses[i].location.display_address[0]
-	Address2 = response.businesses[i].location.display_address[1]
-	City = response.businesses[i].location.city
-	State = response.businesses[i].location.state_code
-	Zipcode =response.businesses[i].location.postal_code
-	Country = response.businesses[i].location.country_code
-	Phone = response.businesses[i].phone
-	StoreHours = ""
-	TimeZone = "EST"
-	DmaCode = ""
-	Status = "Active"
-	csv << [Name, Brand, Lat, Long, Address1, Address2, City, State, Zipcode, Country, Phone, StoreHours, TimeZone, DmaCode, Status]
+	#puts response.businesses[i].location.city
+	if response.businesses[i].location.city == "Medford"
+
+	    Brand = response.businesses[i].name
+
+	    Name = response.businesses[i].name
+		Storeid = "" 
+		Lat = response.businesses[i].location.coordinate.latitude
+		Long = response.businesses[i].location.coordinate.longitude
+
+		#Brand = response.businesses[i].categories[0][0]
+		Address1 = response.businesses[i].location.display_address[0]
+		#Address2 = response.businesses[i].location.display_address[1]
+		Address2 = ""
+		City = response.businesses[i].location.city
+		State = response.businesses[i].location.state_code
+		Zipcode =response.businesses[i].location.postal_code
+		Country = response.businesses[i].location.country_code
+		Phone = response.businesses[i].phone
+		Fax = ""
+		StoreHours = ""
+		TimeZone = "EST"
+		DmaCode = ""
+		Status = "Active"
+		csv << [ Brand, Storeid, Name, Lat, Long, Address1, Address2, City, State, Zipcode, Country, Phone, Fax, StoreHours, TimeZone, DmaCode, Status]
+	end
 end
 #puts x
  x= x+20
